e466be88f6f1b4bb91ffd1f4b3e2a21e7188cb16
bhanudatsinhjhala <jhalabhanudattsinh4090@gmail.com>
2023-05-03 11:27:21 +0530

    added proper comments in code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const net = require("net");
+
+// variable declaration
 const sockets = [];
+
+// function for data event from client
 const handleClientMsg = async (socket) => {
   socket.on("data", (data) => {
     console.log("data", data.toString());
@@ -8,19 +12,34 @@ const handleClientMsg = async (socket) => {
     });
   });
 };
+
+// create server function
 const server = net.createServer((socket) => {
+  // sending default message from server to client on connection
   socket.write(
     JSON.stringify({ name: "Server", message: "Hello from server" })
   );
+
+  // pushing client socket in sockets array
   sockets.push(socket);
+
+  // calling data event function
   handleClientMsg(socket);
+
+  // get total connections / clients connected on server
   server.getConnections((err, count) => {
     if (err) console.log("err", err);
     console.log("count", count);
   });
 });
+
+// method on client connection
 server.on("connection", () => console.log("Server is connected"));
+
+// server listening
 server.listen(4000);
+
+// error handler on server
 server.on("error", (err) => {
   console.log("error", err);
 });
--- src/client.js
+++ src/client.js
@@ -1,20 +1,30 @@
 const net = require("net");
 const inquirer = require("inquirer");
+
+// IIFE for when file is running
 (async () => {
+  // question for asking user for his/her name
   const nameQuestion = {
     type: "input",
     name: "Client Data",
     message: "Please Enter your name --",
   };
+
+  // question for asking user to type message
   const chatQuestion = {
     type: "input",
     name: `chat`,
     message: "Your Message --",
   };
+
+  // creating prompt
   const prompt = inquirer.createPromptModule();
+
+  // asking name question
   const getClientName = await prompt(nameQuestion);
   const clientName = getClientName[nameQuestion.name];
 
+  // asking message question and sending back to server
   const clientChatHandler = async () => {
     const chatMessageAnswer = await prompt(chatQuestion);
     const getChatMessage = chatMessageAnswer[chatQuestion.name];
@@ -35,6 +45,11 @@ const inquirer = require("inquirer");
   client.on("data", (data) => {
     data = JSON.parse(data.toString());
     console.log("\n" + data.name + ":", data.message + "\n");
+
+    /* only ask message question if the data recieved from server
+     * is from client itself
+     * or from server
+     */
     if (data.name === clientName || data.name === "Server") {
       clientChatHandler();
     }
